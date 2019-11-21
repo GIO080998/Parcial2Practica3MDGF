f422efb7f8178c2f72559eac1cd4a54af8b04de8
github_GIO080998 <gio1998fer@gmail.com>
2019-11-21 12:26:59 -0600

    Initial commit

---
+++ app/src/main/java/mdgf/edu/tesoem/itics/parcial2practica3mdgf/MainActivity.java
@@ -0,0 +1,31 @@
+package mdgf.edu.tesoem.itics.parcial2practica3mdgf;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+List<String> listaEstados;
+String[] strsEstados;
+Spinner objSpinner;
+ArrayAdapter<String> listEstados;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        objSpinner = findViewById(R.id.cargalista);
+        strsEstados = new String[] {"CDMX","Estado de Mexico","Veracruz","Sonora","Colima"};
+        listaEstados = new ArrayList<String>();
+
+        Collections.addAll(listaEstados,strsEstados);
+        listEstados = new ArrayAdapter<String>(this,android.R.layout.simple_spinner_item,  listaEstados);
+        objSpinner.setAdapter(listEstados);
+
+    }
+}
